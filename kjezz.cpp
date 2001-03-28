a3865e5da4becbabd62abfe3eaa0612bd3e5e63d
Stefan Schimanski <schimmi@kde.org>
2001-03-28 11:26:48 +0000

    * lifes -> lives
    svn path=/trunk/kdegames/kjezz/; revision=89095

--- kjezz.cpp
+++ kjezz.cpp
@@ -4,7 +4,7 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2 of the License,Life or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -77,7 +77,7 @@ KJezzball::KJezzball()
     m_percentLCD  = new QLCDNumber( 5, m_view );
     infoLayout->addWidget( m_percentLCD );
 
-    label = new QLabel( i18n("Lifes:"), m_view );
+    label = new QLabel( i18n("Lives:"), m_view );
     infoLayout->addWidget( label );
     m_lifesLCD  = new QLCDNumber( 5, m_view );
     infoLayout->addWidget( m_lifesLCD );
