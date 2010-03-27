5bd522b4090386b0be6fdcae8d98c3dec2d7832c
Yuri Chornoivan <yurchor@ukr.net>
2010-03-27 07:05:15 +0000

    typo fix
    svn path=/trunk/KDE/kdegames/kbounce/; revision=1107907

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
   aboutData.addAuthor(ki18n("Benjamin Meyer"), ki18n("Contributions"), "ben+kbounce@meyerhome.net");
   aboutData.addAuthor(ki18n("Tomasz Boczkowski"), ki18n("Port to KDE4. Current maintainer"), "tboczkowski@onet.pl" );
   aboutData.addCredit(ki18n("Dmitry Suzdalev"), ki18n("Port to QGraphicsView framework"), "dimsuz@gmail.com");
-  aboutData.addCredit(ki18n("Andreas Scherf"), ki18n("Image Backgound and Fixes"), "ascherfy@gmail.com");
+  aboutData.addCredit(ki18n("Andreas Scherf"), ki18n("Image Background and Fixes"), "ascherfy@gmail.com");
 
   KCmdLineArgs::init( argc, argv, &aboutData );
 
