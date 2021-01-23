7aa72b11cc3110d3698180d85cecdf3856407b38
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-01-23 03:48:17 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- src/backgroundselector.h
+++ src/backgroundselector.h
@@ -1,23 +1,10 @@
 /*
- * Copyright (C) 2010 Andreas Scherf <ascherfy@gmail.com>
- *
- * This file is part of the KDE project "KBounce"
- *
- * KBounce is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * KBounce is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with KBounce; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- */
+    This file is part of the KDE project "KBounce"
+
+    SPDX-FileCopyrightText: 2010 Andreas Scherf <ascherfy@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 
 #ifndef BACKGROUNDSELECTOR_H
 #define BACKGROUNDSELECTOR_H
--- src/gameobject.cpp
+++ src/gameobject.cpp
@@ -1,23 +1,10 @@
 /*
- * Copyright (C) 2007 Tomasz Boczkowski <tboczkowski@onet.pl>
- *
- * This file is part of the KDE project "KBounce"
- *
- * KBounce is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * KBounce is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with KBounce; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.  
- */
+    This file is part of the KDE project "KBounce"
+
+    SPDX-FileCopyrightText: 2007 Tomasz Boczkowski <tboczkowski@onet.pl>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 
 #include "gameobject.h"
 
